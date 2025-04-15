210da039a78911bfcc68e36a92c7c949ac27dc30
NetureDev <lifecycle702@gmail.com>
2025-04-15 19:27:27 -0400

    Update website.js

--- website.js
+++ website.js
@@ -1,8 +1,15 @@
 const list = document.querySelectorAll('.list');
+const indicator = document.querySelector('.indicator');
+
 function activeLink() {
-    list.forEach((item) =>
-    item.classList.remove('active'));
+    list.forEach((item) => item.classList.remove('active'));
     this.classList.add('active');
 }
-list.forEach((item) =>
-item.addEventListener('click',activeLink));
+
+list.forEach((item, index) => {
+    item.addEventListener('click', function () {
+        activeLink.call(this);
+        // Move the indicator based on index
+        indicator.style.transform = `translateX(calc(70px * ${index}))`;
+    });
+});
